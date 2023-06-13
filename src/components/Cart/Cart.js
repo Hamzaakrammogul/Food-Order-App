6bcc328a066f8a175c8b12a11d1ddd3df78689e4
hamzaakrammogul <hmughal502@gmail.com>
2023-06-13 20:44:48 +0500

    Added https requests and form into the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,16 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import classes from './Cart.module.css';
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = props => {
 
+    const [isCheckOut, setIsCheckOut] =  useState(false);
+
     const ctx = useContext(CartContext);
-
     const totalAmount= `$${ctx.totalAmount.toFixed(2)}`;
-
     const hasItems = ctx.items.length >0;
 
     const cartItemRemoveHandler = id=> {
@@ -17,10 +18,12 @@ const Cart = props => {
     };
 
     const cartItemAddHandler = item => {
-
         ctx.addItem({...item, amount:1})
     };
 
+    const orderHandler = ( ) => {
+        setIsCheckOut(true);
+    }
 
     const cartItems = <ul className={classes['cart-items']}>
         {ctx.items.map((item) => <CartItem key={item.id}
@@ -32,6 +35,11 @@ const Cart = props => {
         />)}
     </ul>
 
+    const modalAction = <div className={classes.actions}>
+    <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+   {hasItems && <button className={classes.button} onClick={orderHandler}>Order</button>}
+</div>
+
     return (
         <Modal onClose={props.onClose}>
             {cartItems}
@@ -39,10 +47,8 @@ const Cart = props => {
                 <span>Total amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-               {hasItems && <button className={classes.button}>Order</button>}
-            </div>
+            {isCheckOut && <Checkout onCancel={props.onClose}/>}
+            {!isCheckOut && modalAction}
         </Modal>
     );
 }
